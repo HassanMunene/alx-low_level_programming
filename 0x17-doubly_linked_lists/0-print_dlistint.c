33be857338e26da8de8d4a2c7494c67109cd294c
HassanMunene <awanzihassan@gmail.com>
2024-03-21 15:58:52 +0300

    have remove all identaions in the file

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,12 +10,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t counter = 0;
-    while (h != NULL)
-    {
-    printf("%d\n", h->n);
-    counter += 1;
-    h = h->next;
-    }
-    return (counter);
+size_t counter = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+counter += 1;
+h = h->next;
+}
+return (counter);
 }
