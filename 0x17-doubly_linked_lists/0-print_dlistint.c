dbcd0ce3d64da2cc2bc08c7ed0f2288ebab8dc7c
HassanMunene <awanzihassan@gmail.com>
2024-03-21 15:54:43 +0300

    have added a description for print_listint function

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,6 +2,12 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * print_dlistint - prints all the elements of a listintt list
+ * @h: pointer to the first node in the list
+ *
+ * Return: the no of nodes in the list
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
     size_t counter = 0;
