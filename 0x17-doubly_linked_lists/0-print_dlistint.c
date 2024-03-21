7e951ecec65897791ceba73115f7af09ae2e6ece
HassanMunene <awanzihassan@gmail.com>
2024-03-21 15:45:03 +0300

    testing my file to see where the style problem iis

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,20 +1,15 @@
+#include <stdlib.h>
 #include "lists.h"
 #include <stdio.h>
 
-/**
- * print_dlistint - prints all the elements of a listintt list
- * @h: pointer to the start of the linked list
- *
- * Return: number of node
- */
 size_t print_dlistint(const dlistint_t *h)
 {
-size_t i;
-
-for (i = 0; h != NULL; i++)
-{
-printf("%d\n", h->n);
-h = h->next;
-}
-return (i);
+    size_t counter = 0;
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        counter += 1;
+        h = h->next;
+    }
+    return (counter);
 }
