c1fb5d8aaf579ac1e6d9a5716a94ce084b5a5591
HassanMunene <awanzihassan@gmail.com>
2024-03-23 16:39:37 +0300

    insert a node at a specific position in the doubly linked liist

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * insert_dnodeint_at_index - add a node at a specific position
+ * @h: pointer to the head pointer that points to head node
+ * @idx: the position at which we want to insert the node
+ * @n: the value of the node
+ *
+ * Return: the address of the new node | NULL if failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+dlistint_t *newNode;
+dlistint_t *current;
+dlistint_t *temp1;
+dlistint_t *temp2;
+
+unsigned int index;
+newNode = malloc(sizeof(dlistint_t));
+if (newNode == NULL)
+{
+return (NULL);
+}
+newNode->n = n;
+current = *h;
+
+for (index = 0; (*current).next != NULL; index++)
+{
+if (index == idx)
+{
+temp1 = (*current).prev;
+temp2 = current;
+}
+current = current->next;
+}
+
+/* this if the idx is the last node in our case*/
+if (index == idx && (*current).next == NULL)
+{
+temp1 = (*current).prev;
+temp2 = current;
+}
+/* if idx is out of range*/
+if (index < idx)
+{
+return (NULL);
+}
+(*temp1).next = newNode;
+(*temp2).prev = newNode;
+(*newNode).prev = temp1;
+(*newNode).next = temp2;
+return (newNode);
+}
