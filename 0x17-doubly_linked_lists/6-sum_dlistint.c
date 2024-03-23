e42fbc8c8dbe019572413492c0d32c96c1b9ea5f
HassanMunene <awanzihassan@gmail.com>
2024-03-23 15:22:14 +0300

    calculate the sum of the values in all the nodes in the doubly linked list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * sum_dlistint - The sum of all ns in node
+ * @head: pointer to the head node
+ *
+ * Return: an integer representing the sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+int sum = 0;
+dlistint_t *current;
+current = head;
+if (head == NULL)
+{
+return (0);
+}
+
+while((*current).next != NULL)
+{
+sum += (*current).n;
+current = current->next;
+}
+sum = sum + (*current).n;
+return (sum);
+}
